9f0ff64303d8b5ed979121ae813ec7d4fdc66198
Ben Zenker <imbenzenker@gmail.com>
2019-05-13 23:34:51 -0400

    added favicon

--- src/components/Page.js
+++ src/components/Page.js
@@ -11,9 +11,19 @@ import SocialLink, { LinksSegment } from './SocialLink'
 export default ({ children }) => (
   <Fragment>
     <Helmet>
+      {/* Favicon */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#25343e" />
+      <meta name="msapplication-TileColor" content="#00aba9" />
+      <meta name="theme-color" content="#ffffff" />
+
       {/* Analytics Tracking Metadata */}
       <script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101182604);</script>
       <script async src="//static.getclicky.com/js"></script>
+
       {/* Font for social icons */}
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
     </Helmet>
