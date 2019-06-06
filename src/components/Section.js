f94ff85e0ba76ad6f6c719365fea04d46240db3a
Ben Zenker <imbenzenker@gmail.com>
2019-06-05 21:30:53 -0400

    external links now open in a new tab

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,79 +1,92 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import MediaGallery from './MediaGallery'
 import { HeroImage, EmbeddedVideoImage, ExternalLinkImage } from './SectionHero'
 import { titleCase } from '../utils/transform'
 
-const Section = ({
-  tag = undefined, thumbnail, body, url, color,
-  additionalMedia, hideTitle,
-}) => {
-  const sectionType = tag.name
-
-  let sectionHero = thumbnail && (
-    <HeroImage fluid={thumbnail.fluid} />
-  )
-
-  if (['video', 'animation'].includes(sectionType)) {
-    sectionHero = <EmbeddedVideoImage url={url} image={sectionHero} />
-  } else if (['sound', 'music'].includes(sectionType)) {
-    // TODO: create component for audio
-    // sectionHero = <EmbeddedAudioImage url={url} />
-  } else if (['code', 'mobile', 'web'].includes(sectionType)) {
-    let icon
-    let action
-    switch (sectionType) {
-      /* eslint default-case: 0 */
-      case 'code':
-        icon = 'code'
-        action = 'View Code'
-        break;
-      case 'mobile':
-        icon='mobile-alt'
-        action='Download App'
-        break;
-      case 'web':
-        icon='globe'
-        action='Visit Website'
-        break;
-    }
-
-    sectionHero = (
-      <ExternalLinkImage
-        icon={icon}
-        action={action}
-        url={url}
-        image={sectionHero}
-      />
-    )
+class Section extends Component {
+  componentDidMount() {
+    const links = document.querySelectorAll('.markdown-body a')
+    const external = Array.from(links)
+      .filter(link => link.hostname !== window.location.hostname)
+    external.forEach((link) => {
+      link.setAttribute('target', '_blank')
+      link.setAttribute('rel', 'noopener')
+    })
   }
 
-  const galleryData = additionalMedia ? (
-    additionalMedia.filter(media => media.file.contentType.includes('image'))
-  ) : (
-    []
-  )
+  render() {
+    const { tag = undefined, thumbnail, body, url, color,
+      additionalMedia, hideTitle } = this.props
 
-  return (
-    <section id={sectionType}>
-      {!hideTitle &&
-        <h3>{titleCase(sectionType)}</h3>
+    const sectionType = tag.name
+
+    let sectionHero = thumbnail && (
+      <HeroImage fluid={thumbnail.fluid} />
+    )
+
+    if (['video', 'animation'].includes(sectionType)) {
+      sectionHero = <EmbeddedVideoImage url={url} image={sectionHero} />
+    } else if (['sound', 'music'].includes(sectionType)) {
+      // TODO: create component for audio
+      // sectionHero = <EmbeddedAudioImage url={url} />
+    } else if (['code', 'mobile', 'web'].includes(sectionType)) {
+      let icon
+      let action
+      switch (sectionType) {
+        /* eslint default-case: 0 */
+        case 'code':
+          icon = 'code'
+          action = 'View Code'
+          break;
+        case 'mobile':
+          icon='mobile-alt'
+          action='Download App'
+          break;
+        case 'web':
+          icon='globe'
+          action='Visit Website'
+          break;
       }
 
-      {sectionHero}
-
-      {body &&
-        <div
-          style={{ marginTop: '1rem' }}
-          dangerouslySetInnerHTML={{
-            __html: body.childMarkdownRemark.html
-          }}
+      sectionHero = (
+        <ExternalLinkImage
+          icon={icon}
+          action={action}
+          url={url}
+          image={sectionHero}
         />
-      }
+      )
+    }
 
-      <MediaGallery media={galleryData} color={color} />
-    </section>
-  )
+    const galleryData = additionalMedia ? (
+      additionalMedia.filter(media => media.file.contentType.includes('image'))
+    ) : (
+      []
+    )
+
+    return (
+      <section id={sectionType}>
+        {!hideTitle &&
+          <h3>{titleCase(sectionType)}</h3>
+        }
+
+        {sectionHero}
+
+        {body &&
+          <div
+            className="markdown-body"
+            style={{ marginTop: '1rem' }}
+            dangerouslySetInnerHTML={{
+              __html: body.childMarkdownRemark.html
+            }}
+          />
+        }
+
+        <MediaGallery media={galleryData} color={color} />
+      </section>
+    )
+  }
 }
 
 export default Section
