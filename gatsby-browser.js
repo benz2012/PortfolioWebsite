24d872bbb67fa7c0eb48637b9be9332283e5aca1
Ben Zenker <imbenzenker@gmail.com>
2019-06-06 09:38:21 -0400

    eliminated scrolling error

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,8 @@ exports.onInitialClientRender = () => {
   if (hash.length > 1) {
     const id = hash.substring(1)
     const elm = document.getElementById(id)
-    elm.scrollIntoView()
+    if (elm) {
+      elm.scrollIntoView()
+    }
   }
 }
