2938092d48f93b9a69f124481ef6a18cbfe19861
Ben Zenker <imbenzenker@gmail.com>
2019-06-05 21:35:48 -0400

    making home button more clear

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -6,11 +6,16 @@ import TextLink from './TextLink'
 const HeaderName = styled.h1`
   margin-top: 1rem;
   margin-bottom: 3rem;
+  text-decoration: underline;
+
+  &:hover {
+    color: grey;
+  }
 `
 
 const HeaderNav = () => (
   <TextLink to="/">
-    <HeaderName>← Ben Zenker</HeaderName>
+    <HeaderName>Ben Zenker</HeaderName>
   </TextLink>
 )
 
